87e5db9a9a6c02cb81fd428784366d3c1d727bd7
NikolaMX <nikola.mixailov@abv.bg>
2025-11-25 21:20:39 +0200

    added maps

---
+++ ui/mods/medieval_mode/maplist.js
@@ -0,0 +1,5 @@
+﻿cShareSystems.load_pas("Haunted Mayhem Mode", [
+	"coui://ui/mods/medieval_mode/systems/alloy_haunted_mayhem.pas",
+	"coui://ui/mods/medieval_mode/systems/roc_haunted_mayhem.pas",
+	"coui://ui/mods/medieval_mode/systems/superbowl_haunted_mayhem.pas"
+]);
