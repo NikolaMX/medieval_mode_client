5b03b39e251047b4662b2622db80ff18badccee4
NikolaMX <nikola.mixailov@abv.bg>
2025-12-20 23:32:11 +0200

    added lock and updated rock

--- ui/mods/medieval_mode/maplist.js
+++ ui/mods/medieval_mode/maplist.js
@@ -1,5 +1,6 @@
 ﻿cShareSystems.load_pas("Haunted Mayhem Mode", [
 	"coui://ui/mods/medieval_mode/systems/alloy_haunted_mayhem.pas",
 	"coui://ui/mods/medieval_mode/systems/roc_haunted_mayhem.pas",
-	"coui://ui/mods/medieval_mode/systems/superbowl_haunted_mayhem.pas"
+	"coui://ui/mods/medieval_mode/systems/superbowl_haunted_mayhem.pas",
+	"coui://ui/mods/medieval_mode/systems/lock_haunted_mayhem.pas"
 ]);
