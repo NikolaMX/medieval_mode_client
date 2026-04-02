f23e30cfec8189475baa8491573b13bf7b28438b
NikolaMX <nikola.mixailov@abv.bg>
2026-04-02 16:28:06 +0300

    udpated maps

--- ui/mods/medieval_mode/maplist.js
+++
@@ -1,9 +0,0 @@
-﻿cShareSystems.load_pas("Haunted Mayhem Mode", [
-	"coui://ui/mods/medieval_mode/systems/alloy_haunted_mayhem.pas",
-	"coui://ui/mods/medieval_mode/systems/roc_haunted_mayhem.pas",
-	"coui://ui/mods/medieval_mode/systems/superbowl_haunted_mayhem.pas",
-	"coui://ui/mods/medieval_mode/systems/lock_haunted_mayhem.pas",
-	"coui://ui/mods/medieval_mode/systems/amplus_haunted_mayhem.pas",
-	"coui://ui/mods/medieval_mode/systems/mukden_haunted_mayhem.pas",
-	"coui://ui/mods/medieval_mode/systems/shoreline_haunted_mayhem.pas"
-]);
---
+++ ui/mods/medieval_mode_client/maplist.js
@@ -0,0 +1,9 @@
+﻿cShareSystems.load_pas("Haunted Mayhem Mode", [
+	"coui://ui/mods/medieval_mode_client/systems/alloy_haunted_mayhem.pas",
+	"coui://ui/mods/medieval_mode_client/systems/roc_haunted_mayhem.pas",
+	"coui://ui/mods/medieval_mode_client/systems/superbowl_haunted_mayhem.pas",
+	"coui://ui/mods/medieval_mode_client/systems/lock_haunted_mayhem.pas",
+	"coui://ui/mods/medieval_mode_client/systems/amplus_haunted_mayhem.pas",
+	"coui://ui/mods/medieval_mode_client/systems/mukden_haunted_mayhem.pas",
+	"coui://ui/mods/medieval_mode_client/systems/shoreline_haunted_mayhem.pas"
+]);
